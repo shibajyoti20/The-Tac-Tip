f4b0fd4d4ddcd972c9e220dc520081713e709044
hrk2023 <hrkdasgupta025@gmail.com>
2021-05-15 18:15:19 +0530

    Mobile Optimised Banner and Catalogue Section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,8 @@ import './Navbar.css';
 function Navbar()
 {
     return (
-        <div className="navbar-wrapper">
+        <>
+        {/* <div className="navbar-wrapper">
             <div className="brand-wrapper">
                 <a href="#">
                     <img src={BrandLogo} className="Logo" alt="BrandLogo"/>
@@ -23,7 +24,34 @@ function Navbar()
                     contact
                 </li>
             </ul>
+        </div> */}
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={BrandLogo} className="Logo" alt="BrandLogo"/>
+                        <img src={BrandName} className="Name" alt="BrandName"/>
+                    </a>
+                    <button className="navbar-toggler" type="button" 
+                    data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav" style={{"width":"40%"}}>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
+      </>
     );
 }
 
